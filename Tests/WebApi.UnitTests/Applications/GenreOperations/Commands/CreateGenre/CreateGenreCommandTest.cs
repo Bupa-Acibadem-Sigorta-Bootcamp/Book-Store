ef011e74aa41c96a196e4f96020856ea10053b00
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2022-01-10 16:03:18 +0300

    Create genre servisi için aynı tür mevcut olmasında durumunda hata mesajı fırlatır testi yazıldı.

--- Tests/WebApi.UnitTests/Applications/GenreOperations/Commands/CreateGenre/CreateGenreCommandTest.cs
+++ Tests/WebApi.UnitTests/Applications/GenreOperations/Commands/CreateGenre/CreateGenreCommandTest.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using FluentAssertions;
+using WebApi.Applications.GenreOperations.Commands.CreateGenre;
+using WebApi.DataBaseOpeOperations;
+using WebApi.Entities;
+using WebApi.UnitTests.TestSetup;
+using Xunit;
 
 namespace WebApi.UnitTests.Applications.GenreOperations.Commands.CreateGenre
 {
-    public class CreateGenreCommandTest
+    public class CreateGenreCommandTest : IClassFixture<CommanTestFixture>
     {
+        private readonly BookStoreDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CreateGenreCommandTest(CommanTestFixture commanTestFixture)
+        {
+            _context = commanTestFixture.context;
+            _mapper = commanTestFixture.Mapper;
+        }
+        [Fact]
+        public void WhenTryingToAddaGenreWiththeSameName_inValidation_ShouldbeReturnanErrorMessage()
+        {
+            var genre = new Genre()
+            {
+                IsActive = true,
+                Name = "Fizik"
+            };
+            _context.Add(genre);
+            _context.SaveChanges();
+
+            CreateGenreCommand command = new CreateGenreCommand(_context, _mapper);
+            command.Model = new CreateGenreViewModel() { Name = genre.Name };
+
+            FluentActions
+                .Invoking(() => command.Handle())
+                .Should().Throw<InvalidOperationException>()
+                .And.Message.Should().Be("Kitap Türü Zaten Mevcut!");
+
+        }
     }
 }
